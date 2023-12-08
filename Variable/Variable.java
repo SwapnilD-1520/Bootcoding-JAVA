eb763eb630dd54b8ffa097fcdb30f9ce0d506c92
SwapnilD-1520 <dandekarswapnil108@gmail.comgit config --global user.name SwapnilD-1520cat .git/configSwapnilD-1520git config --global user.email dandekarswapnil108@gmail.com>
2023-12-08 16:23:16 +0530

    A1- Code Of Final variable

--- Variable/Variable.java
+++
@@ -1,9 +0,0 @@
-class Variable{
-public static void main(String args[])
-{
-   int myOne = 20;
-   float myTwo= 4.2f;
-System.out.println("myOne=" + myOne);
-System.out.println("myTwo="+ myTwo);
-}
-}
