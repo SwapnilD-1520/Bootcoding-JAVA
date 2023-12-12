1b1682ca45d6bc436d6bd10dceefd6a22b5305b9
SwapnilD-1520 <dandekarswapnil108@gmail.comgit config --global user.name SwapnilD-1520cat .git/configSwapnilD-1520git config --global user.email dandekarswapnil108@gmail.com>
2023-12-12 14:39:08 +0530

    Assignment of factorial

---
+++ Loops/Factorial.java
@@ -0,0 +1,16 @@
+//Create a program to calculate and print the factorial of a number entered by the user using a for loop
+
+class Factorial
+{
+	public static void main(String args[])
+	{
+	
+		int fact = 1;
+		for( int i=1; i<=5; i++)
+	{
+		fact =fact*i;
+	}	
+		System.out.println("factorial for Number 5 =" + fact);
+		
+	}	
+}
