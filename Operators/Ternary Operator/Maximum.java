23fc53c64ad4e40e0250e8668ee8627525e78f8c
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2023-12-13 18:58:21 +0530

    Assignment - Ternary Opertor

---
+++ Operators/Ternary Operator/Maximum.java
@@ -0,0 +1,12 @@
+//Implement a program to find the maximum of two numbers using the ternary operator.
+
+class Maximum
+{	
+	public static void main(String args[])
+	{	
+	 	int p = 2;
+		int q = 4;
+		String result = p > q ? "Maximum"  : "Minimum" ;
+		System.out.println("Number is = "+ result );
+	}	
+}
