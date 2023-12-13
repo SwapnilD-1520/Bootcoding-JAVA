1fcf9f25f62c0881fa0b5651468bd70f302ed814
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2023-12-13 18:34:09 +0530

    Assignmemt - Reverse Numbers From 10 to 1

---
+++ Operators/Increment & Decrement Operator/CountDown.java
@@ -0,0 +1,22 @@
+//Create a Java program to display the countdown from 10 to 1 using the post-decrement operator.
+
+class CountDown
+{	
+	public static void main(String args[])
+	{
+		int k;
+		
+		System.out.println("The countdown are =");
+
+		for(k = 10; k>=1; k--)
+		{	
+			System.out.println( k );
+		}
+		
+	}	
+}
+
+
+
+
+
