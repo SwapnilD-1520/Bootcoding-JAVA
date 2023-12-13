f63b17937eafa96cfd3e327daeb19dbdd5be9e1b
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2023-12-13 14:58:22 +0530

    Code For Printing Table 7

---
+++ Loops/TableSeven.java
@@ -0,0 +1,11 @@
+//Write a program that prints the multiplication table of 7 using a for loop
+
+class TableSeven
+{
+	public static void main(String args[])
+	{
+		for(int i=1; i<=10; i++)
+		System.out.println("The given table for seven =" +(i*7));
+	}	
+
+}
