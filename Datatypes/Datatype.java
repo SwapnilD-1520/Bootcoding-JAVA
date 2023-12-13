c8c1aa3a9670455c97ac185ded5e5b2ff3dca4a5
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2023-12-13 14:34:47 +0530

    Assignment COde for Datatypes

---
+++ Datatypes/Datatype.java
@@ -0,0 +1,35 @@
+class Datatype
+{
+public static void main(String args[]){
+//single line comment
+/* multiple
+line 
+comment
+*/
+System.out.println("Hello Coder");
+
+byte myByte=123;
+System.out.println("byte value:"+myByte); //string concatination by using + operator
+
+short myShort=12345;
+System.out.println("short value:"+myShort);
+
+int myInt=1234567;
+System.out.println("int value:"+myInt);
+
+long myLong=123456789;
+System.out.println("long value:"+myLong);
+
+float myFloat=1.23f;
+System.out.println("float value:"+myFloat);
+
+double myDouble=2.154615;
+System.out.println("double value:"+myDouble);
+
+char myChar='A';
+System.out.println("char value:"+myChar);
+
+boolean myBoolean=true;
+System.out.println("boolean value:"+myBoolean);
+}
+}
