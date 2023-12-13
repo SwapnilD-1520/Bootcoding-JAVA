c95f73ded347e9c1e6750e04ecc9aee1ab973caa
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2023-12-13 15:12:40 +0530

    Assignment Code - Arithmatic Operator

---
+++ Operators/Arithmatic Operator/Arithmatic.java
@@ -0,0 +1,13 @@
+class Arithmatic
+{
+	public static void main(String args[])
+	{
+		int m = 1;
+		int n = 5;
+		
+		System.out.println("Addition of two numbers = " + (m + n));
+		System.out.println("Subtraction of two numbers = " + (m - n));
+		System.out.println("Multiplication of two numbers = " + (m * n));
+		System.out.println("Divison of two numbers = " + (m / n));
+	}	
+}
