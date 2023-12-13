bf15fa6e2b0479012c155df1937320c616572496
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2023-12-13 16:06:25 +0530

    Assignment - Solve Using Assignmemt op

---
+++ Operators/Assignment Operator/Assignment.java
@@ -0,0 +1,23 @@
+//Write a Java program to demonstrate the use of various assignment operators (+=, -=, *=, /=).
+
+class Assignment
+{		
+	public static void main(String args[])
+	
+	{	
+		int y = 50;
+		y += 20;	
+
+		System.out.println("Add & assign =" + y);
+		y -= 20;	
+
+		System.out.println("Subtract & assign =" + y);
+		y *= 20;	
+
+		System.out.println("multiply & assign =" + y);
+		y /= 20;	
+
+		System.out.println("Divide & assign =" + y);
+	}	
+	
+}
