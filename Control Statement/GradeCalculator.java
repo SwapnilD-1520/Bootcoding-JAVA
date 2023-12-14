a78d321a83444e37dd1fa1e8d31a2729f1c4a1c8
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2023-12-14 13:50:40 +0530

    Assignment - Calculate Grade For students

---
+++ Control Statement/GradeCalculator.java
@@ -0,0 +1,29 @@
+//Create a program that calculates the final grade for a student based on their scores in exams, assignments, and participation.
+
+class GradeCalculator
+{
+	public static void main(String args[])
+
+	{
+		int score = 90;		
+	
+	if(score == 90)
+	{
+		System.out.println(" The student has A Grade ");
+	}
+	else if( score <= 80)
+	{
+		System.out.println(" The student has B Grade ");
+	}
+	else if( score <= 50)
+	{
+		System.out.println(" The student has C Grade ");
+	}
+	else
+	{	
+		System.out.println(" The student is fail ");
+	}
+	
+	}
+
+}
