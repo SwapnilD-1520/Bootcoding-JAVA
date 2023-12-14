add32fc5d1e009aea377706e083b268e3ebd1c30
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2023-12-14 14:28:35 +0530

    Assignment - Body Mass Index Calculator

---
+++ Control Statement/BmiCalculator.java
@@ -0,0 +1,26 @@
+//Build a BMI (Body Mass Index) calculator that takes a person's height and weight as input and categorizes them as underweight, normal weight, overweight, or obese.
+class BmiCalculator
+{
+	public static void main(String args[])
+	{
+		int height = 5;
+		int weight = 50;
+		if( height == 5 && weight == 50 )
+		{
+				System.out.println("The Person is normalWeight ");
+		}
+		else if( height <= 4 && weight <= 40)
+		{
+				System.out.println("The Person is in underweight ");
+		}
+		else if( height >= 6 && weight >= 55)
+		{
+				System.out.println("The Person is in overweight  ");
+		}
+		else
+		{
+				System.out.println("The Person is in obese ");
+		}
+}
+		
+}
