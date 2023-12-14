79621d9e4d193aa04403b49b8f5da6f919319213
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2023-12-14 14:47:05 +0530

    Assignment - Code For Number Properties

---
+++ Control Statement/NumberProperties.java
@@ -0,0 +1,25 @@
+//Implement a program that takes an integer as input and checks whether it's prime, even, odd.
+class NumberProperties 
+{
+	public static void main(String args[])
+	{
+		int num = 23;
+		{
+			System.out.println(" The Given value is = " + num );
+			
+		}
+		if( num % 2 == 0)
+		{
+			System.out.println(" The Number is even ");
+		
+		}
+		else if( num % 2 == 1)
+		{
+			System.out.println("The Number is odd ");
+		}
+		else 
+		{
+			System.out.println(" The Number is prime ");
+		}
+	}	
+}
