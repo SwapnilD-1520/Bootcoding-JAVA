9bff295e412c5f43c521ffcc1eccfa61dd87aa7f
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2023-12-15 22:09:32 +0530

    Assignment - Code For Weather Adviser

---
+++ Control Statement/WeatherAdviser.java
@@ -0,0 +1,18 @@
+//Write a program that takes the current temperature as input and advises the user on what to wear (e.g., shorts and t-shirt for high temperatures, coat for cold temperatures).
+ 
+ class WeatherAdviser 
+{
+	public static void main(String args[])
+	{
+		float temp = 36.9f;
+		if( temp > 36.8)
+		{
+			System.out.println(" wear Shorts & t-shirt ");
+		}
+		else
+		{
+				System.out.println(" wear coat ");
+		}
+	}
+	
+ }
